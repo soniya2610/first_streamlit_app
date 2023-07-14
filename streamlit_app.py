7ec6ff53fa6086c046e2576a4b64862e5cff859a
soniya2610 <139368833+soniya2610@users.noreply.github.com>
2023-07-14 09:10:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 streamlit.title('My parents new healthy diner')
-streamlit.header('Breakfast menu')
-streamlit.text('Eggs')
-streamlit.text('Pancakes')
-streamlit.text('Waffles')
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
